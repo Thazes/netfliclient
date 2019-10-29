ac348001bea3d3ab079d089133fc946fc0ea3f2b
Thazes <nyahumphrey@gmail.com>
2019-10-29 20:33:34 +0300

    feignclient setup

--- src/main/java/com/netfliclient/demo/model/Movie.java
+++ src/main/java/com/netfliclient/demo/model/Movie.java
@@ -1,22 +1,36 @@
 package com.netfliclient.demo.model;
 
 public class Movie {
-    private int Movieid;
-    private String movieName;  //for recommending movies
-    private String movieCategory;
+    private int id;
+    private int yearofRelease;
+    private String movieName;
+    private String contentOwner;
+    private String classification;  //category
+    private String type;            //original or suggested
 
-    public Movie(int movieid, String movieName, String movieCategory) {
-        Movieid = movieid;
+    public Movie(int id, int yearofRelease, String movieName, String contentOwner, String classification, String type) {
+        this.id = id;
+        this.yearofRelease = yearofRelease;
         this.movieName = movieName;
-        this.movieCategory = movieCategory;
+        this.contentOwner = contentOwner;
+        this.classification = classification;
+        this.type = type;
     }
 
-    public int getMovieid() {
-        return Movieid;
+    public int getId() {
+        return id;
     }
 
-    public void setMovieid(int movieid) {
-        Movieid = movieid;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYearofRelease() {
+        return yearofRelease;
+    }
+
+    public void setYearofRelease(int yearofRelease) {
+        this.yearofRelease = yearofRelease;
     }
 
     public String getMovieName() {
@@ -27,11 +41,27 @@ public class Movie {
         this.movieName = movieName;
     }
 
-    public String getMovieCategory() {
-        return movieCategory;
+    public String getContentOwner() {
+        return contentOwner;
     }
 
-    public void setMovieCategory(String movieCategory) {
-        this.movieCategory = movieCategory;
+    public void setContentOwner(String contentOwner) {
+        this.contentOwner = contentOwner;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
