4d747e94132164b16a1d0abb8f7ece838a31d217
milleradulu <milleradulu@gmail.com>
2019-10-31 06:36:49 +0300

    Implement Feign Client methods for interaction with API

---
+++ src/main/java/com/netfliclient/demo/feign/NetflixFeign.java
@@ -0,0 +1,38 @@
+package com.netfliclient.demo.feign;
+
+import com.netfliclient.demo.model.Category;
+import com.netfliclient.demo.model.Movie;
+import com.netfliclient.demo.model.MovieSuggestion;
+import com.netfliclient.demo.model.Subscriber;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient(name = "subscribers", url = "http://localhost:9000", configuration = FeignConfig.class)
+public interface NetflixFeign {
+    @RequestMapping(method = RequestMethod.POST, value = "subscribers")
+    Subscriber newSubscriber(@RequestBody Subscriber subscriber);
+
+    @RequestMapping(method = RequestMethod.GET, value = "categories")
+    List<Category> allCategories();
+
+    @RequestMapping(method = RequestMethod.GET, value = "movies")
+    List<Movie> allMovies();
+
+    @RequestMapping(method = RequestMethod.GET, value = "movies/{categoryId}?type={movieType}")
+    List<Movie> categoryMovies(@PathVariable Long categoryId, @PathVariable String movieType);
+
+    @RequestMapping(method = RequestMethod.POST, value = "subscribers/{identificationNumber}/movies")
+    Movie suggestMovie(@PathVariable String identificationNumber, @RequestBody MovieSuggestion movieSuggestion);
+
+    @RequestMapping(method = RequestMethod.GET, value = "subscribers/{subscriberId}/movies")
+    List<Movie> subscriberMovies(@PathVariable Long subscriberId);
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "subscribers/{identificationNumber}/movies/{movieId}")
+    Movie updateMovie(@PathVariable String identificationNumber, @PathVariable Long movieId, @RequestBody MovieSuggestion movieSuggestion);
+
+}
---
+++ src/main/java/com/netfliclient/demo/runners/NetflixRunner.java
@@ -0,0 +1,88 @@
+package com.netfliclient.demo.runners;
+
+import com.netfliclient.demo.feign.NetflixFeign;
+import com.netfliclient.demo.model.Category;
+import com.netfliclient.demo.model.Movie;
+import com.netfliclient.demo.model.MovieSuggestion;
+import com.netfliclient.demo.model.Subscriber;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Random;
+
+@Component
+public class NetflixRunner implements CommandLineRunner {
+    private final NetflixFeign netflixFeign;
+    private final Random random = new Random();
+
+    public NetflixRunner(NetflixFeign netflixFeign) {
+        this.netflixFeign = netflixFeign;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String[] movieTypes = new String[]{"suggested", "original"};
+        // Register Subscriber
+        Subscriber latestAccount = netflixFeign.newSubscriber(
+                new Subscriber("331245", "Hello From The Other Side")
+        );
+
+        System.out.println("Latest Account: " + latestAccount.toString());
+
+        // Fetch all movie categories
+        List<Category> allCategories = netflixFeign.allCategories();
+
+        for (Category c : allCategories) {
+            System.out.println("Category " + c.getId() + ": " + c.getCategoryName());
+        }
+
+        // Fetch all movies
+        List<Movie> allMovies = netflixFeign.allMovies();
+
+        for (Movie m : allMovies) {
+            System.out.println("Movie " + m.getId() + ": " + m.toString());
+        }
+
+        // Fetch category movies
+        List<Movie> allNewSubMovies = netflixFeign.categoryMovies(
+                Integer.toUnsignedLong(allCategories.get(
+                        random.nextInt(allCategories.size())
+                ).getId()),
+                movieTypes[random.nextInt(movieTypes.length)]
+        );
+
+        System.out.println("Categorized Movies");
+        for (Movie m : allMovies) {
+            System.out.println("Movie " + m.getId() + ": " + m.toString());
+        }
+
+        // Suggest movie
+        Movie suggestedMovie = netflixFeign.suggestMovie(
+                latestAccount.getIdentificationNumber(),
+                new MovieSuggestion("Sound of Music", "2003", allCategories.subList(0, 2))
+        );
+
+        System.out.println("Suggested Movie: " + suggestedMovie.toString());
+
+        // Subscriber movies
+        List<Movie> subscriberMovies = netflixFeign.subscriberMovies(
+                Integer.toUnsignedLong(latestAccount.getId())
+        );
+
+        System.out.println("All Subscriber Movies");
+        for (Movie m : subscriberMovies) {
+            System.out.println("Movie " + m.getId() + ": " + m.toString());
+        }
+
+        // Update movie
+        Movie updatedMovie = netflixFeign.updateMovie(
+                latestAccount.getIdentificationNumber(),
+                Integer.toUnsignedLong(suggestedMovie.getId()),
+                new MovieSuggestion("No Longer Sound of Music", "2005", allCategories.subList(0, 2))
+        );
+
+        System.out.println("Updated Movie: " + updatedMovie.toString());
+
+    }
+}
